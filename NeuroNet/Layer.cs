8860d104f2dcb6e41b7fcf294d37714690089110
dextrinne <167472397+dextrinne@users.noreply.github.com>
2025-11-22 15:19:21 +0300

    Add files via upload

---
+++ NeuroNet/Layer.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using static System.Math;
+
+namespace MO_32_2_Chuprova_BoxBox.NeuroNet
+{
+    abstract class Layer
+    {
+        // Модификатор protected работает аналогично private, но необходим для внутрииерархического использования членов
+        // По умолчанию private
+
+        // Поля
+        protected string name_Layer;
+        string pathDirWeights; // Путь к каталогу, где находится файл синаптических весов для нейросети
+        string pathFileWeights; // Путь к файлу синаптических весов для нейросети
+        protected int numofneurons; // Число нейронов текущего слоя
+        protected int numofprevneurons; // Число нейронов предыдущего слоя
+        protected const double learningrate = 0.031d; // Скорость обучения (подбираем сами: 041155 0.029383 0.031) - коэффициент обновления весов
+        protected const double momentum = 0.2d; // Момент инерции (подбираем сами: 0.00465 0.2) - помогает избежать локальных минимумов
+        protected double[,] lastdeltaweights; // Веса предыдущей итерации обучения
+        protected Neuron[] neurons; // Массив нейронов текущего слоя
+
+
+        // Свойства
+        public Neuron[] Neurons { get => neurons; set => neurons = value; } // Массив нейронного слоя
+        public double[] Data // Передача входных данных на нейроны слоя и активация нейрона
+        {
+            set
+            {
+                // Передаем входные данные каждому нейрону и активируем его
+                for (int i = 0; i < numofneurons; i++)
+                {
+                    Neurons[i].Activator(value);
+                }
+            }
+        }
+
+        // Конструктор
+        protected Layer(int non, int nopn, NeuronType nt, string nm_layer)
+        {
+            numofneurons = non; // Количество нейронов текущего слоя
+            numofprevneurons = nopn; // Количество нейронов предыдущего слоя
+            Neurons = new Neuron[non]; // Определение массива нейронов
+            name_Layer = nm_layer;
+
+            // Формирование путей для хранения весов
+            pathDirWeights = AppDomain.CurrentDomain.BaseDirectory + "memory\\";
+
+            // Формирование полного пути к файлу весов
+            pathFileWeights = pathDirWeights + name_Layer + "_memory.csv";
+
+            lastdeltaweights = new double[non, nopn + 1];
+
+            double[,] Weights; // Временный массив синаптических весов текущего слоя
+
+            if (File.Exists(pathFileWeights))
+                Weights = WeightsInitialize(MemoryMode.GET, pathFileWeights);
+            else
+            {
+                Directory.CreateDirectory(pathDirWeights);
+                Weights = WeightsInitialize(MemoryMode.INIT, pathFileWeights);
+            }
+
+            for (int i = 0; i < non; i++)
+            {
+                double[] tmp_weights = new double[nopn + 1];
+                for (int j = 0; j < nopn + 1; j++)
+                {
+                    tmp_weights[j] = Weights[i, j];
+                }
+                Neurons[i] = new Neuron(tmp_weights, nt); // Заполнение массива нейронами
+            }
+        }
+
+        // Направляем массив сюда
+        public double[,] WeightsInitialize(MemoryMode mm, string path)
+        {
+            Random random = new Random();
+
+            char[] delim = new char[] { ';', ' ' };
+            string[] tmpStrWeights;
+            double[,] weights = new double[numofneurons, numofprevneurons + 1];
+
+            // Выбор режима работы с памятью
+            switch (mm)
+            {
+                // Режим загрузки весов
+                case MemoryMode.GET:
+                    tmpStrWeights = File.ReadAllLines(path);
+                    string[] memory_element;
+
+                    for (int i = 0; i < numofneurons; i++)
+                    {
+                        memory_element = tmpStrWeights[i].Split(delim);
+                        for (int j = 0; j < numofprevneurons + 1; j++)
+                        {
+                            weights[i, j] = double.Parse(memory_element[j].Replace(',', '.'),
+                                System.Globalization.CultureInfo.InvariantCulture);
+                        }
+                    }
+                    break;
+
+                // Режим сохранения весов в файл
+                case MemoryMode.SET:
+                    string strSET = "";
+                    for (int i = 0; i < numofneurons; i++)
+                    {
+                        strSET += Neurons[i].Weights[0].ToString(System.Globalization.CultureInfo.InvariantCulture);
+                        for (int j = 1; j < numofprevneurons + 1; j++)
+                        {
+                            strSET += ";" + Neurons[i].Weights[j].ToString(System.Globalization.CultureInfo.InvariantCulture);
+                        }
+                        if (i < numofneurons - 1)
+                            strSET += "\n";
+                    }
+                    File.WriteAllText(path, strSET);
+                    break;
+
+                // Режим инициализации случайными весами
+                case MemoryMode.INIT:
+                    weights = RandomInit(numofneurons, numofprevneurons + 1);
+                    weights = SrChanger(numofneurons, numofprevneurons + 1, weights);
+                    break;
+
+
+            }
+            return weights;
+        }
+
+        // Случайная инициализация весов в диапазоне [-1, +1]
+        protected double[,] RandomInit(int a, int b)
+        {
+            double[,] weights = new double[a, b];
+            Random random = new Random();
+
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    weights[i, j] = random.NextDouble() * 2 - 1;
+                }
+            }
+            return weights;
+        }
+
+        // Нормализация весов: создаёт отклонение = 1 и мат. ожидание = 0
+        // Согласно лекции: "Матожидание = 0, Стандартное отклонение = 1"
+        protected double[,] SrChanger(int a, int b, double[,] cweights)
+        {
+            double[,] weights = cweights;
+
+            for (int i = 0; i < a; i++)
+            {
+                // Вычисляем среднее (матожидание)
+                double mean = 0;
+                for (int j = 0; j < b; j++)
+                {
+                    mean += weights[i, j];
+                }
+                mean /= b;
+
+                // Центрируем (матожидание = 0)
+                for (int j = 0; j < b; j++)
+                {
+                    weights[i, j] -= mean;
+                }
+
+                // Вычисляем стандартное отклонение
+                double stdDev = 0;
+                for (int j = 0; j < b; j++)
+                {
+                    stdDev += weights[i, j] * weights[i, j];
+                }
+                stdDev = Math.Sqrt(stdDev / b);
+
+                // Нормализуем (stdDev = 1)
+                if (stdDev > 0)
+                {
+                    for (int j = 0; j < b; j++)
+                    {
+                        weights[i, j] /= stdDev;
+                    }
+                }
+            }
+            return weights;
+        }
+
+        abstract public void Recognize(Network net, Layer nextLayer); // Для прямых проходов
+        abstract public double[] BackwardPass(double[] stuff); // Для обратных проходов
+    }
+}
---
+++ NeuroNet/Network.cs
@@ -0,0 +1,141 @@
+﻿namespace MO_32_2_Chuprova_BoxBox.NeuroNet
+{
+    // Функция активации: Гиперболический тангенс, 15-72-32-10
+    class Network
+    {
+        // Все слои сети
+        private InputLayer input_Layer = null;
+        private HiddenLayer hidden_Layer1 = new HiddenLayer(72, 15, NeuronType.Hidden, nameof(hidden_Layer1));
+        private HiddenLayer hidden_Layer2 = new HiddenLayer(32, 72, NeuronType.Hidden, nameof(hidden_Layer2));
+        private OutputLayer output_Layer = new OutputLayer(10, 32, NeuronType.Output, nameof(output_Layer));
+
+        private double[] fact = new double[10]; // Массив фактического выхода сети
+        private double[] e_error_avr; // Среднее значение энергии ошибки эпохи обучения
+
+        // Свойства
+        public double[] Fact { get => fact; } // Массив фактического выхода сети
+
+        // Среднее значение энергии ошибки эпохи обучения
+        public double[] E_error_avr { get => e_error_avr; set => e_error_avr = value; }
+
+        // Конструктор
+        public Network() { }
+
+        // Прямой проход сети
+        public void ForwardPass(Network net, double[] netInput)
+        {
+            net.hidden_Layer1.Data = netInput;
+            net.hidden_Layer1.Recognize(null, net.hidden_Layer2);
+            net.hidden_Layer2.Recognize(null, net.output_Layer);
+            net.output_Layer.Recognize(net, null);
+        }
+
+        // Обучение нейросети
+        public void Train(Network net)
+        {
+            net.input_Layer = new InputLayer(NetworkMode.Train); // Инициализация входного слоя
+            int epoches = 15; // Количество эпох обучения (сами настраиваем (до 20))
+            double tmpSumError; // Временная переменная суммы ошибок (столько же сколько и нейронов)
+            double[] errors; //  Вектор (массив) сигнала ошибки выходного слоя
+            double[] temp_gsums1; // Вектор градиента 1-ого скрытого слоя
+            double[] temp_gsums2; // Вектор градиента 2-ого скрытого слоя
+
+            e_error_avr = new double[epoches];
+            for (int k = 0; k < epoches; k++) // Перебор эпох обучения
+            {
+                e_error_avr[k] = 0;
+                net.input_Layer.Shuffling_Array_Rows(net.input_Layer.Trainset); // Перетасовка обучающей выборки
+
+                for (int i = 0; i < net.input_Layer.Trainset.GetLength(0); i++)
+                {
+                    double[] tmpTrain = new double[15]; // Обучающий образ
+                    for (int j = 0; j < tmpTrain.Length; j++)
+                    {
+                        tmpTrain[j] = net.input_Layer.Trainset[i, j + 1]; 
+                    }
+                    // Прямой проход обучающего образа
+                    ForwardPass(net, tmpTrain);
+
+                    // Вычисление ошибки по итерации
+                    tmpSumError = 0; // Для каждого обучающего образа среднее значение ошибки этого образа равна 0 
+                    errors = new double[net.fact.Length]; // Переопределение массива сигнала ошибки выходного слоя
+                    for (int x = 0; x < errors.Length; x++)
+                    {
+                        if (x == net.input_Layer.Trainset[i, 0])
+                        {
+                            errors[x] = 1.0 - net.fact[x];
+                        }
+                        else
+                        {
+                            errors[x] = - net.fact[x]; // errors[x] = 0 - net.fact[x];
+                        }
+                        tmpSumError += errors[x] * errors[x] / 2;
+                    }
+                    e_error_avr[k] += tmpSumError / errors.Length; // Суммарное значение энергии ошибки k-ой ошибки
+
+                    // Обратный проход и коррекция весов
+                    temp_gsums2 = net.output_Layer.BackwardPass(errors);
+                    temp_gsums1 = net.hidden_Layer2.BackwardPass(temp_gsums2);
+                    net.hidden_Layer1.BackwardPass(temp_gsums1);
+                }
+                e_error_avr[k] /= net.input_Layer.Trainset.GetLength(0); // Среднее значение энергии ошибки одной эпохи
+            }
+
+            net.input_Layer = null; // Обнуление слоя
+
+            // Запись скорректированных весов
+            net.hidden_Layer1.WeightsInitialize(MemoryMode.SET, nameof(hidden_Layer1) + "_memory.csv");
+            net.hidden_Layer2.WeightsInitialize(MemoryMode.SET, nameof(hidden_Layer2) + "_memory.csv");
+            net.output_Layer.WeightsInitialize(MemoryMode.SET, nameof(output_Layer) + "_memory.csv");
+        }
+
+        public void Test(Network net)
+        {
+            net.input_Layer = new InputLayer(NetworkMode.Test); // Инициализация входного слоя
+            int epoches = 2; // Количество эпох обучения (сами настраиваем (от 2 до 5))
+            double tmpSumError; // Временная переменная суммы ошибок (столько же сколько и нейронов)
+            double[] errors; //  Вектор (массив) сигнала ошибки выходного слоя
+            double[] temp_gsums1; // Вектор градиента 1-ого скрытого слоя
+            double[] temp_gsums2; // Вектор градиента 2-ого скрытого слоя
+
+            e_error_avr = new double[epoches];
+            for (int k = 0; k < epoches; k++) // Перебор эпох обучения
+            {
+                e_error_avr[k] = 0;
+                net.input_Layer.Shuffling_Array_Rows(net.input_Layer.Testset); // Перетасовка обучающей выборки
+
+                for (int i = 0; i < net.input_Layer.Testset.GetLength(0); i++)
+                {
+                    double[] tmpTrain = new double[15]; // Обучающий образ
+                    for (int j = 0; j < tmpTrain.Length; j++)
+                    {
+                        tmpTrain[j] = net.input_Layer.Testset[i, j + 1];
+                    }
+                    // Прямой проход обучающего образа
+                    ForwardPass(net, tmpTrain);
+
+                    // Вычисление ошибки по итерации
+                    tmpSumError = 0; // Для каждого обучающего образа среднее значение ошибки этого образа равна 0 
+                    errors = new double[net.fact.Length]; // Переопределение массива сигнала ошибки выходного слоя
+                    for (int x = 0; x < errors.Length; x++)
+                    {
+                        if (x == net.input_Layer.Testset[i, 0])
+                        {
+                            errors[x] = 1.0 - net.fact[x];
+                        }
+                        else
+                        {
+                            errors[x] = -net.fact[x]; // errors[x] = 0 - net.fact[x];
+                        }
+                        tmpSumError += errors[x] * errors[x] / 2;
+                    }
+                    e_error_avr[k] += tmpSumError / errors.Length; // Суммарное значение энергии ошибки k-ой ошибки
+                }
+                e_error_avr[k] /= net.input_Layer.Testset.GetLength(0); // Среднее значение энергии ошибки одной эпохи
+            }
+
+            net.input_Layer = null; // Обнуление слоя
+
+        }
+    }
+}
